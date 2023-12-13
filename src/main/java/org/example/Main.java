58cb261a30e26a640d73be6968ad7d8fea3b383b
keet <keet@huelsdau.com>
2023-12-13 11:59:52 +0100

    main.java changed text

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,8 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
 
         int count = 0;
-        System.out.println("Jetzt gehts endlich los");
+        int a = 5;
+        System.out.println("Jetzt gehts endlich los" + a);
         while (true) {
             Thread.sleep(2 * 1000);
             System.out.println(format("App running continuously ... %d", count++));
