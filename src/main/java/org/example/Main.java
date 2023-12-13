ba45df0103b2cd51fdac94ee4b6de653bb9b31f8
keet <keet@huelsdau.com>
2023-12-13 09:46:58 +0100

    abändern system out text1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,14 @@
 package org.example;
+
 import static java.lang.String.format;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
 
-        int count =0;
+        int count = 0;
+        System.out.println("Jetzt gehts endlich los");
         while (true) {
-            Thread.sleep(2*1000);
-            System.out.println("Jetzt gehts los");
+            Thread.sleep(2 * 1000);
             System.out.println(format("App running continuously ... %d", count++));
         }
     }
